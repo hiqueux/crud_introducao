913cbc38078cf7821e47a94a774aff1e51cd84f6
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-06-03 09:40:17 -0300

    docs - documentado a configuração e conexão do banco em connect.php

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,11 +1,12 @@
 <?php
+    // inicia ou recupera a sessão atual do usuário.
     session_start();
-
+    // define as credenciais de conexão com o banco de dados.
     $host = "localhost";
     $user = "root";
     $pass = "root";
     $db = "sistema_simples";
-    
+    // estabelece a conexão com o banco de dados usando as credenciais fornecidas.
     $conn = new mysqli($host,$user,$pass,$db);
 
     // if($conn->connect_error){
