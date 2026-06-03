433c39d2c87b65b39cda07528a0a3d2fea1700fa
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-06-03 09:31:52 -0300

    docs - documentado processo de logout e encerramento de sessão em logout.php

--- public/logout.php
+++ public/logout.php
@@ -1,6 +1,9 @@
 <?php
+    // inicia ou recupera a sessão atual do usuário.
     session_start();
+    // remove todas as variáveis de sessão e dados associados.
     session_destroy();
+    // redireciona o usuário para a página de login logout.
     header("Location: ../index.php");
     exit();
 ?>
