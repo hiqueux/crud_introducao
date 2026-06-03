f0f0416bfd9daf6a71636d1834cd6fa02fd77a10
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-06-03 08:31:06 -0300

    docs - docuemntado o fluxo de autenticação e formulário de login em index.php

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
 
+// inclui a conexão com o banco de dados.
 include("infra/db/connect.php");
 
+// verifica se o formulário foi enviado.
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
+    // ele recebe os dados do formulário.
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
 
+    // consulta usuário e senha no banco.
     $sql = "SELECT * FROM users 
     WHERE username = '$usuario' 
     AND password = '$senha'";
 
+    // vai executar a consulta no banco de dados.
     $resultado = $conn -> query($sql);
 
+    // verifica se o usuário foi encontrado.
     if($resultado -> num_rows > 0){
         $_SESSION["usuario"] = $usuario;
         header("Location: public/home.php");
@@ -31,9 +37,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login com PHP</title>
 </head>
 <body>
-     <?php
+    <?php
+    // inclui o componente responsável pela exibição da tabela de usuários.
     include("../public/component/table.php");
     ?>
+    <!-- Formulário de autenticação -->
     <h2>Login com PHP</h2><form method="POST">
         <label for="usuario">Usuario:</label>
         <input type="text" name="usuario">
