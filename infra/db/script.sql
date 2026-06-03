90b8a9e4635eddeaf6beb89b20e8acf73e13dcdf
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-06-03 09:37:36 -0300

    docs - documentado a criação do banco de dados e tabela de usuários em script.sql

--- infra/db/script.sql
+++ infra/db/script.sql
@@ -1,11 +1,13 @@
+-- criação do banco de dados e tabela de usuários.
 CREATE DATABASE sistema_simples;
-
+-- seleciona o banco de dados para uso.
 USE sistema_simples;
-
-CREATE TABLE usuario (
+-- cria a tabela de usuários.
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(255) NOT NULL,
-    senha VARCHAR(255) NOT NULL
+    -- campos para armazenar usuário e senha.
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
-
-INSERT INTO usuario (usuario, senha) VALUE ('admin','123');
+-- insere um usuário padrão na tabela de usuários.
+INSERT INTO users (username, password) VALUES ('admin','123');
