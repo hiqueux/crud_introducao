ba8ff34c8c270b7314f22afdfcf66d454b02e872
hiqueux <henrique_tonioti@estudante.sesisenai.org.br>
2026-06-03 08:54:13 -0300

    docs - documentado a consulta e exibição de usuários em formato de tabela em table.php

--- public/component/table.php
+++ public/component/table.php
@@ -12,12 +12,16 @@
 
     <?php
     
-
+    // consulta SQL para buscar todos os usuários cadastrados no banco.
     $sqlUsuarios = "SELECT * FROM users";
 
+    // executa a consulta no banco de dados.
     $resultadoUsuarios = $conn -> query($sqlUsuarios);
 
+    // verifica se foram encontrados usuários.
     while($linha = $resultadoUsuarios->fetch_assoc()){
+        
+        // exibe os dados de cada usuário em linha de tabela.
         echo "<tr>
         
             <td>" . $linha["id"] . "</td>
